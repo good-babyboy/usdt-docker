91265b0dfb36657eea6885119be38cd67c5af8be
bigbro1024 <bigbro1024@outlook.com>
2022-06-08 02:00:11 +0800

    .

--- services/ruoyi-admin/startup.sh
+++ services/ruoyi-admin/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 server_name="tom"
-
+# 切换到jar目录启动，目的配置外置
+cd /home/ruoyi/jar
 # 启动
 java -jar -Duser.timezone=GMT+08 -server -XX:+HeapDumpOnOutOfMemoryError -Xms512m -Xmx512m $server_name.jar
